6e409adad9cc1be769a2812bece971bc8afe03b2
schragman <schragman@gmail.com>
2023-02-25 14:55:10 +0100

    initial Commit

---
+++ src/test/java/net/schrage/javasoapclient/JavasoapclientApplicationTests.java
@@ -0,0 +1,13 @@
+package net.schrage.javasoapclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavasoapclientApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
